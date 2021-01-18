e90c77ad233480e0ea2a43d0d3529323b348cda6
HirokiMiyakura <miyakurahiroki@gmail.com>
2021-01-18 13:57:18 +0900

    re-refact wc.rb

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -20,24 +20,23 @@ def format_value(value)
 end
 
 def print_values(*args)
-  print args.join(' ')
+  puts args.join(' ')
 end
 
 opts = ARGV.getopts('l')
 line_count_only = opts['l']
 filenames = ARGV
 
-if filenames == []
+if filenames.empty?
   input = $stdin.read
-  line_count = format_value(count_lines(input))
-  word_count = format_value(count_words(input))
-  byte_count = format_value(count_bytes(input))
+  line_count = count_lines(input)
+  word_count = count_words(input)
+  byte_count = count_bytes(input)
   if line_count_only
-    print line_count
+    puts format_value(line_count)
   else
-    print_values(line_count, word_count, byte_count)
+    print_values(format_value(line_count), format_value(word_count), format_value(byte_count))
   end
-  print "\n"
 end
 
 total_line_count = 0
@@ -45,29 +44,24 @@ total_word_count = 0
 total_byte_count = 0
 filenames.each do |filename|
   text = File.read(filename)
-  line_count = format_value(count_lines(text))
+  line_count = count_lines(text)
+  word_count = count_words(text)
+  byte_count = count_bytes(text)
   if line_count_only
-    print_values(line_count, filename)
+    print_values(format_value(line_count), filename)
   else
-    word_count = format_value(count_words(text))
-    byte_count = format_value(count_bytes(text))
-    print_values(line_count, word_count, byte_count, filename)
+    print_values(format_value(line_count), format_value(word_count), format_value(byte_count), filename)
   end
-  print "\n"
 
-  total_line_count += line_count.to_i
-  total_word_count += word_count.to_i
-  total_byte_count += byte_count.to_i
+  total_line_count += line_count
+  total_word_count += word_count
+  total_byte_count += byte_count
 end
 
 if filenames.size >= 2
-  total_line_count = format_value(total_line_count)
-  total_word_count = format_value(total_word_count)
-  total_byte_count = format_value(total_byte_count)
   if line_count_only
-    print_values(total_line_count, 'total')
+    print_values(format_value(total_line_count), 'total')
   else
-    print_values(total_line_count, total_word_count, total_byte_count, 'total')
+    print_values(format_value(total_line_count), format_value(total_word_count), format_value(total_byte_count), 'total')
   end
-  print "\n"
 end
